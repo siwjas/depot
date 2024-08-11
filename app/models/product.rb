062829931cd2957a29b488e0589b24a0140d58f9
Wellington Almeida <wjas.ma@gmail.com>
2024-08-10 21:54:41 -0300

    First commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,22 @@
 class Product < ApplicationRecord
-    validates :title, :description, :image_url, presence: true
-    validates :title, uniqueness: true
-		validates :title, length: { minimum: 5, message: 'must be equal to or greater than 5 characters.' }
-    validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-    validates :image_url, allow_blank: true, format: {
-        with: %r{\.(gif|jpg|png)\z}i, 
-        message: 'must be a URL for GIF, JPG or PNG image.'
-    }
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
+
+	validates :title, :description, :image_url, presence: true
+	validates :title, uniqueness: true
+	validates :title, length: { minimum: 5, message: 'must be equal to or greater than 5 characters.' }
+	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+	validates :image_url, allow_blank: true, format: {
+			with: %r{\.(gif|jpg|png)\z}i, 
+			message: 'must be a URL for GIF, JPG or PNG image.'
+	}
+
+	private
+
+		def ensure_not_referenced_by_any_line_item
+			unless line_items.empty?
+				errors.add(:base, 'Line Items present!')
+				throw :abort
+			end
+		end
 end
