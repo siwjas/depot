107393b0bbae64d6f3a8c79470b2504e368cc404
w311 <wjas.ma@gmail.com>
2024-08-16 02:45:00 -0300

    add hotwire on products

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -20,10 +20,20 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
       post line_items_url, params: { product_id: products(:ruby).id }
     end
 
-		follow_redirect!
+    follow_redirect!
 
-    assert_select 'h2', 'Your Pragmatic Cart'
-		assert_select 'li', "1 \u00D7 Programming Ruby 1.9"
+    assert_select 'h2', 'Your Cart'
+    assert_select 'td', "Programming Ruby 1.9"
+  end
+
+  test "shold create line-item via turbo-stream" do
+    assert_difference("LineItem.count") do
+      post line_items_url, params: { product_id: products(:ruby).id },
+      as: :turbo_stream
+    end
+
+    assert_response :success
+    assert_match /<tr class="line-item-highlight">/, @response.body     
   end
 
   test "should show line_item" do
